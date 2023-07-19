ad7859353ffef599738b4652c10b803e827e3bb2
Tjips <thomas@scheepers.com>
2023-07-19 10:49:48 +0200

    JavaScript bug fixes

--- public/script.js
+++ public/script.js
@@ -9,26 +9,31 @@ function backWindow() {
 var slider = document.querySelector('.time-value.slider');
 var drawTimeValue = document.getElementById('draw-time-value');
 
-// Add an event listener to the slider to detect changes
-slider.addEventListener('input', function() {
-  // Update the text content of the draw-time-value element with the slider's value
-  drawTimeValue.textContent = slider.value;
-});
+if(slider){
+  // Add an event listener to the slider to detect changes
+  slider.addEventListener('input', function() {
+    // Update the text content of the draw-time-value element with the slider's value
+    drawTimeValue.textContent = slider.value;
+  });
+}
 
 collectionPopup = document.getElementById("collection")
-document.getElementById("bagIcon").addEventListener("click", toggleCollection)
-document.getElementById("closeIcon").addEventListener("click", toggleCollection)
 
-document.addEventListener('keydown', function(event) {
-    // Check if the escape key was pressed
-    if (event.key === 'Escape') {
-      // Do something when the escape key is pressed
-      toggleCollection()
-    }
-});
+if(collectionPopup) {
+  document.getElementById("bagIcon").addEventListener("click", toggleCollection)
+  document.getElementById("closeIcon").addEventListener("click", toggleCollection)
 
-function toggleCollection() {
-    collectionPopup.classList.toggle("active")
+  document.addEventListener('keydown', function(event) {
+      // Check if the escape key was pressed
+      if (event.key === 'Escape') {
+        // Do something when the escape key is pressed
+        toggleCollection()
+      }
+  });
+
+  function toggleCollection() {
+      collectionPopup.classList.toggle("active")
+  }
 }
 
 let keyDownInteraction = document.querySelectorAll('input');
