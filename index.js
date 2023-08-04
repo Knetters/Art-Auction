0f8bfa368f8fd4c45825f3e49be9c7a214b97202
Tjips <thomas@scheepers.com>
2023-08-04 23:42:53 +0200

    Progressie

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(cookieParser());
 
 // Lobby storage
 const lobbies = {};
+const drawTime = 120;
 
 // Generate a random 6-digit code for the lobby
 function generateCode() {
@@ -66,12 +67,13 @@ app.get('/findGame', (req, res) => {
 app.get('/game/:gameCode', (req, res) => {
   const iconValue = "ingameIcons";
   const gameCode = req.params.gameCode;
+  const drawTime = req.params.drawTime;
 
   if (lobbies[gameCode]) {
     const players = lobbies[gameCode].players;
     const playerRole = req.cookies.playerRole;
 
-    res.render('game', { iconValue, gameCode, players, playerRole });
+    res.render('game', { iconValue, gameCode, players, playerRole, drawTime });
   } else {
     res.send('Game lobby not found');
   }
@@ -80,32 +82,35 @@ app.get('/game/:gameCode', (req, res) => {
 app.get('/lobby/:lobbyCode', (req, res) => {
   const iconValue = "ingameIcons";
   const lobbyCode = req.params.lobbyCode;
+  const drawTime = req.params.drawTime;
 
   if (lobbies[lobbyCode]) {
     const players = lobbies[lobbyCode].players;
     const playerRole = req.cookies.playerRole;
 
-    res.render('lobby', { iconValue, gameCode: lobbyCode, players, playerRole });
+    res.render('lobby', { iconValue, gameCode: lobbyCode, players, playerRole, drawTime });
   } else {
     res.send('Lobby not found');
   }
 });
 
 io.on('connection', (socket) => {
+  
   socket.on('joinLobby', (data) => {
-    const { playerId, playerRole, playerName, lobbyCode } = data;
-
+    const { playerId, playerRole, playerName, lobbyCode, drawTime } = data;
+  
     if (lobbies[lobbyCode]) {
       if (lobbies[lobbyCode].players.length < 8) {
         const player = { id: playerId, role: playerRole, name: playerName };
         lobbies[lobbyCode].players.push(player);
-
+        lobbies[lobbyCode].drawTime = drawTime; // Store the draw time in the lobby
+  
         socket.join(lobbyCode);
-
+  
         socket.emit('setRole', playerRole);
-
+  
         socket.emit('lobbyRedirect', { lobbyCode });
-
+  
         console.log(`playerId: ${playerId} joined lobby: ${lobbyCode}`);
       } else {
         socket.emit('lobbyFullError');
@@ -114,11 +119,12 @@ io.on('connection', (socket) => {
       socket.emit('lobbyNotFoundError');
     }
   });
+  
 
   socket.on('startGame', (lobbyCode) => {
     if (lobbies[lobbyCode]) {
       const gameCode = lobbyCode;
-      io.to(lobbyCode).emit('navigateToGame', gameCode);
+      io.to(lobbyCode).emit('navigateToGame', gameCode, drawTime);
     }
   });
 
